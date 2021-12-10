ef52d871b2a04d13a54abd462166f370dc851e2a
Farid <88771610+Farid69600@users.noreply.github.com>
2021-12-10 11:55:38 +0100

    Exercice 4

--- Angular/eval-angular/src/app/app.component.ts
+++ Angular/eval-angular/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Statistique } from 'models/statistique';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  statFilm = new Statistique('fa1f5f40', 'Film Statistique', '100ID');
+  statJeu = new Statistique('fa1f5f41', 'Jeu Statistique', '101ID');
+
   title = 'eval-angular';
 }
