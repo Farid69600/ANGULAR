d68f928c00823a7d756d74ae61d246117db386a9
Farid <88771610+Farid69600@users.noreply.github.com>
2021-12-10 15:07:01 +0100

    Exercice 6

--- Angular/eval-angular/src/app/app.component.ts
+++ Angular/eval-angular/src/app/app.component.ts
@@ -7,8 +7,17 @@ import { Statistique } from 'models/statistique';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  statFilm = new Statistique('fa1f5f40', 'Film Statistique', '100ID');
-  statJeu = new Statistique('fa1f5f41', 'Jeu Statistique', '101ID');
+  public tabStat: Statistique[] = [
+    new Statistique('fa1f5f40', 'Film Statistique', '100ID'),
+    new Statistique('fa1f5f41', 'Jeu Statistique', '101ID'),
+  ];
+
+  constructor() {
+    setTimeout(() => {
+      let stat3 = new Statistique('fa1f5f42', 'Cours Angular', '101IE');
+      this.tabStat.push(stat3);
+    }, 5000);
+  }
 
   title = 'eval-angular';
 }
